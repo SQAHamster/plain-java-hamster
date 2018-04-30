e56f62fc2d63430dc79767ea7f2e34dab27f2caf
Steffen Becker <steffen.becker@informatik.uni-stuttgart.de>
2018-04-30 04:17:38 -0700

    Mindmap for PSE contents

---
+++ planning/contents/PSE.mm
@@ -0,0 +1,46 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1525086706413" ID="ID_314305588" MODIFIED="1525086956848" TEXT="PSE">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1525086733197" ID="ID_1867105211" MODIFIED="1525086956840" POSITION="right" TEXT="Programmierung">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1525086739632" ID="ID_159395997" MODIFIED="1525086956841" TEXT="Grundlagen">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1525086796354" ID="ID_1878480970" MODIFIED="1525086956841" TEXT="Elementare Datentypen">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1525086806523" ID="ID_198913274" MODIFIED="1525086956842" TEXT="Anweisungen">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1525086814810" ID="ID_1128255999" MODIFIED="1525086956843" TEXT="Prozeduren">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1525086822782" ID="ID_1998051987" MODIFIED="1525086956843" TEXT="Parameter und Parameterwerte">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1525086833814" ID="ID_1113859824" MODIFIED="1525086956844" TEXT="R&#xfc;ckgabetypen">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1525086751027" ID="ID_1012380887" MODIFIED="1525086956845" TEXT="Objektorientierung">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1525086857234" ID="ID_1669003901" MODIFIED="1525086956846" TEXT="Bibliotheken">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1525086764426" FOLDED="true" ID="ID_334636755" MODIFIED="1525086956847" POSITION="left" TEXT="Softwareentwicklung">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1525086879758" ID="ID_510126589" MODIFIED="1525086956848" TEXT="">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
