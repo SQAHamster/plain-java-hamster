1061f8e86bfa5a32636cf10c780405f274420d53
Steffen Becker <steffen.becker@informatik.uni-stuttgart.de>
2018-08-08 09:23:08 +0200

    Replaced command

--- code/OOPHamster/OOPHamster/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/hamster/commands/InitHamsterCommand.java
+++ code/OOPHamster/OOPHamster/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/hamster/commands/InitHamsterCommand.java
@@ -4,21 +4,23 @@ import java.util.Optional;
 import java.util.stream.IntStream;
 
 import de.unistuttgart.iste.rss.oo.hamstersimulator.commands.CompositeBaseCommand;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.commands.PropertyCommandSpecification;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.commands.PropertyCommandSpecification.ActionKind;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.commands.PropertyMap;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.commands.UnidirectionalUpdatePropertyCommand;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes.Direction;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.hamster.Hamster;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.territory.tile.Grain;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.territory.tile.Tile;
-import de.unistuttgart.iste.rss.oo.hamstersimulator.territory.tile.commands.SetCurrentTileCommand;
 
 public class InitHamsterCommand extends CompositeBaseCommand {
 
     public InitHamsterCommand(final PropertyMap<Hamster> hamsterState, final Optional<Tile> newTile, final Direction newDirection, final int newGrainCount) {
         super();
         this.compositeCommandBuilder.add(
-                new SetCurrentTileCommand<Hamster>(hamsterState, Optional.empty()),
+                new UnidirectionalUpdatePropertyCommand<Hamster>(hamsterState, new PropertyCommandSpecification("currentTile", Optional.empty(), ActionKind.SET)),
                 new SetDirectionCommand(hamsterState, newDirection),
-                new SetCurrentTileCommand<Hamster>(hamsterState, newTile));
+                new UnidirectionalUpdatePropertyCommand<Hamster>(hamsterState, new PropertyCommandSpecification("currentTile", newTile, ActionKind.SET)));
         IntStream.range(1, newGrainCount).forEach(i -> this.compositeCommandBuilder.add(new AddGrainCommand(hamsterState, new Grain())));
     }
 
--- code/OOPHamster/OOPHamster/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/simulator/commands/MoveCommand.java
+++ code/OOPHamster/OOPHamster/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/simulator/commands/MoveCommand.java
@@ -2,12 +2,14 @@ package de.unistuttgart.iste.rss.oo.hamstersimulator.simulator.commands;
 
 import java.util.Optional;
 
+import de.unistuttgart.iste.rss.oo.hamstersimulator.commands.PropertyCommandSpecification;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.commands.PropertyCommandSpecification.ActionKind;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.commands.PropertyMap;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.commands.UnidirectionalUpdatePropertyCommand;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes.Location;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes.LocationVector;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.hamster.Hamster;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.territory.tile.Tile;
-import de.unistuttgart.iste.rss.oo.hamstersimulator.territory.tile.commands.SetCurrentTileCommand;
 
 public class MoveCommand extends HamsterCompositeBaseCommand {
 
@@ -24,7 +26,7 @@ public class MoveCommand extends HamsterCompositeBaseCommand {
         assert this.hamster.getCurrentTerritory().isLocationInTerritory(newHamsterPosition);
         final Tile newTile = this.hamster.getCurrentTerritory().getTileAt(newHamsterPosition);
 
-        builder.add(new SetCurrentTileCommand<Hamster>(this.hamsterState, Optional.of(newTile)));
+        builder.add(new UnidirectionalUpdatePropertyCommand<Hamster>(this.hamsterState, new PropertyCommandSpecification("currentTile", Optional.of(newTile), ActionKind.SET)));
     }
 
 }
