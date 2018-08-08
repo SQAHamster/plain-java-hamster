2a31936c6e2c3382085d4f2adbb69f537e6cfa53
Steffen Becker <steffen.becker@informatik.uni-stuttgart.de>
2018-08-08 09:44:54 +0200

    Replaced remove content command

--- code/OOPHamster/OOPHamster/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/simulator/commands/PickGrainCommand.java
+++ code/OOPHamster/OOPHamster/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/simulator/commands/PickGrainCommand.java
@@ -1,11 +1,13 @@
 package de.unistuttgart.iste.rss.oo.hamstersimulator.simulator.commands;
 
+import de.unistuttgart.iste.rss.oo.hamstersimulator.commands.PropertyCommandSpecification;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.commands.PropertyCommandSpecification.ActionKind;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.commands.PropertyMap;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.commands.UnidirectionalUpdatePropertyCommand;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.hamster.Hamster;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.hamster.commands.AddGrainCommand;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.territory.tile.Grain;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.territory.tile.Tile;
-import de.unistuttgart.iste.rss.oo.hamstersimulator.territory.tile.commands.RemoveContentsCommand;
 
 public class PickGrainCommand extends HamsterCompositeBaseCommand {
 
@@ -22,7 +24,7 @@ public class PickGrainCommand extends HamsterCompositeBaseCommand {
         this.currentTile = this.hamster.getCurrentTerritory().getTileAt(this.hamster.getCurrentTile().get().getLocation());
         this.pickedGrain = this.currentTile.getAnyContentOfType(Grain.class);
         builder.add(
-                new RemoveContentsCommand(this.currentTile.getState(), this.pickedGrain),
+                new UnidirectionalUpdatePropertyCommand<Tile>(this.currentTile.getState(), new PropertyCommandSpecification("content", this.pickedGrain, ActionKind.REMOVE)),
                 new AddGrainCommand(hamsterState, pickedGrain));
     }
 
