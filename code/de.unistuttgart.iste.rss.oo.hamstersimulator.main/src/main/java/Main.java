2b8f33b35c35824a50e243635f9cf017dc105a48
Steffen Becker <steffen.becker@informatik.uni-stuttgart.de>
2018-08-28 10:58:42 +0200

    Renamed project

---
+++ code/de.unistuttgart.iste.rss.oo.hamstersimulator.main/src/main/java/Main.java
@@ -0,0 +1,45 @@
+import de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes.Direction;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes.Location;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.external.model.Hamster;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.external.model.HamsterGame;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.external.model.Territory;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.ui.javafx.JavaFXUI;
+
+public class Main {
+
+    public static void main(final String[] args) {
+        JavaFXUI.start();
+
+        final HamsterGame game = new HamsterGame();
+        game.initialize();
+
+        game.displayInNewGameWindow();
+        game.runGame(Main::exampleRun);
+    }
+
+    public static void exampleRun(final Territory territory) {
+        final Hamster paule = territory.getDefaultHamster();
+        final Hamster willi = new Hamster(territory, Location.from(1, 9), Direction.WEST, 0);
+        final Hamster marry = new Hamster(territory, Location.from(1, 2), Direction.EAST, 0);
+
+        paule.write("Hallo Steffi!");
+        while (!paule.grainAvailable() && paule.frontIsClear()) {
+            paule.move();
+        }
+        while (paule.grainAvailable()) {
+            paule.pickGrain();
+        }
+        paule.turnLeft();
+        paule.turnLeft();
+        while (paule.frontIsClear()) {
+            paule.move();
+        }
+        while (!paule.mouthEmpty()) {
+            paule.putGrain();
+        }
+        willi.move();
+        willi.write("Hallo auch von mir!");
+        marry.move();
+        marry.write("Ich auch!");
+    }
+}
---
+++ code/de.unistuttgart.iste.rss.oo.hamstersimulator.main/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/external/model/Territory.java
@@ -0,0 +1,40 @@
+package de.unistuttgart.iste.rss.oo.hamstersimulator.external.model;
+
+import de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes.Location;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.internal.model.territory.GameTerritory;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.internal.model.territory.TerritoryLoader;
+
+public class Territory {
+
+    private final HamsterGame game;
+    private final GameTerritory internalTerritory;
+    private final Hamster defaultHamster;
+
+    public Territory(final HamsterGame game) {
+        super();
+        this.game = game;
+        this.internalTerritory = new GameTerritory();
+        this.defaultHamster = Hamster.fromInternalDefaultHamster(this);
+    }
+
+    public Hamster getDefaultHamster() {
+        return this.defaultHamster;
+    }
+
+    public HamsterGame getGame() {
+        return this.game;
+    }
+
+    public /*@ pure @*/ boolean isLocationInTerritory(final Location location) {
+        return this.internalTerritory.isLocationInTerritory(location);
+    }
+    
+    public void loadFromFile(final String territoryFile) {
+        TerritoryLoader.initializeFor(this.internalTerritory).loadFromFile(territoryFile);
+    }
+
+    GameTerritory getInternalTerritory() {
+        return this.internalTerritory;
+    }
+
+}
