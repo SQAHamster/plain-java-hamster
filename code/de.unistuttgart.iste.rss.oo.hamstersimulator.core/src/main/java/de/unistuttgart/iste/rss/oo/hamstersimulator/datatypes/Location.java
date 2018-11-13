87a37e520a8d300f7c8632a3c0aca2a996349b78
Steffen Becker <steffen.becker@informatik.uni-stuttgart.de>
2018-11-13 09:41:37 +0100

    Javadoc and some JML annotations added.

--- code/de.unistuttgart.iste.rss.oo.hamstersimulator.core/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/datatypes/Location.java
+++ code/de.unistuttgart.iste.rss.oo.hamstersimulator.core/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/datatypes/Location.java
@@ -4,20 +4,36 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
- * Immutable class representing a location in the hamster territory.
+ * Immutable class representing a location in the hamster territory. Locations
+ * are made of a row and a column number.
  * @author Steffen Becker
  *
  */
 public final class Location {
 
     /**
-     * Constant representing the origin of the coordinate system. 
+     * Constant representing the origin (0,0) of the coordinate system. 
      */
     public static final Location ORIGIN = Location.from(0,0);
 
+    /**
+     * Variable to store the row number.
+     */
     private final int row;
+    
+    /**
+     * Variable to store the column number.
+     */
     private final int column;
 
+    /** 
+     * Create a new location. The rows and column numbers of locations
+     * in the hamster territory start by (0,0) and increase only to
+     * positive integers. Larger rows are further down. Larger columns
+     * are further right.
+     * @param row The non-negative row count.
+     * @param column The non-negative column count.
+     */
     public Location(final int row, final int column) {
         super();
         if (row < 0 || column < 0) {
@@ -27,14 +43,23 @@ public final class Location {
         this.column = column;
     }
 
-    public /*@ pure @*/ int getRow() {
+    /**
+     * @return The non-negative row number of this location.
+     */
+    public /*@ pure, helper @*/ int getRow() {
         return row;
     }
 
-    public /*@ pure @*/ int getColumn() {
+    /**
+     * @return The non-negative column number of this location.
+     */
+    public /*@ pure, helper @*/ int getColumn() {
         return column;
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -44,6 +69,9 @@ public final class Location {
         return result;
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
     @Override
     public boolean equals(final Object obj) {
         if (this == obj) {
@@ -65,14 +93,38 @@ public final class Location {
         return true;
     }
 
-    public Location translate(final LocationVector movementVector) {
-        return new Location(this.row + movementVector.getDeltaRow(), this.column + movementVector.getDelteColumn());
+    /**
+     * Creates a new location object by using this location object
+     * as base point and adding the given location vector to it in
+     * a mathematical sense.
+     * @param movementVector A {@link LocationVector} to be added to this location
+     * @return A new location object which is the result of adding the given
+     *      location vector to this location. Will never be null.
+     */
+    public /*@ pure, helper @*/ Location translate(final LocationVector movementVector) {
+        return new Location(this.row + movementVector.getDeltaRow(), this.column + movementVector.getDeltaColumn());
     }
 
+    /** 
+     * Convienience method to create a new location. The rows and column numbers of locations
+     * in the hamster territory start by (0,0) and increase only to
+     * positive integers. Larger rows are further down. Larger columns
+     * are further right.
+     * @param row The non-negative row count.
+     * @param column The non-negative column count.
+     */    
     public static Location from(final int row, final int column) {
         return new Location(row, column);
     }
 
+    /**
+     * This method creates a {@link Stream} of locations which enumerate the locations
+     * formed by the box with the from location as upper, left corner and the to location
+     * as the lower, right one. The box' locations are enumerated row by row.
+     * @param from Upper left corner of the box to be enumerated in the stream.
+     * @param to Lower right corner of the box to be enumerated in the stream.
+     * @return The stream which enumerates the locations in the specified box.
+     */
     public static Stream<Location> getAllLocationsFromTo(final Location from, final Location to) {
         final Stream<Stream<Location>> stream = IntStream.range(from.getRow(), to.getRow()+1).mapToObj(row -> IntStream.range(from.getColumn(), to.getColumn()+1).mapToObj(column -> Location.from(row, column)));
         return stream.flatMap(s -> s);
--- code/de.unistuttgart.iste.rss.oo.hamstersimulator.core/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/datatypes/LocationVector.java
+++ code/de.unistuttgart.iste.rss.oo.hamstersimulator.core/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/datatypes/LocationVector.java
@@ -1,27 +1,65 @@
 package de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes;
 
+/**
+ * A location vector is a vector in the mathematical sense.
+ * It can be used to describe a location relative to another
+ * location.
+ * 
+ * @author Steffen Becker
+ *
+ */
 public final class LocationVector {
+    
+    /**
+     * The vector's delta in the row number. Can be any integer number.
+     */
     private final int deltaRow;
-    private final int delteColumn;
+    
+    /**
+     * The vector's deleta in the column number. Can be any integer number.
+     */
+    private final int deltaColumn;
+    
+    /**
+     * Creates a new location vector with the given vector deltas.
+     * @param deltaRow The delta in the row count. Can be any integer.
+     * @param delteColumn The delta in the column count. Can be any integer.
+     */
     public LocationVector(final int deltaRow, final int delteColumn) {
         super();
         this.deltaRow = deltaRow;
-        this.delteColumn = delteColumn;
+        this.deltaColumn = delteColumn;
     }
-    public int getDeltaRow() {
+    
+    /**
+     * @return The delta of the row this vector describes.
+     */
+    public /*@ pure, helper @*/ int getDeltaRow() {
         return deltaRow;
     }
-    public int getDelteColumn() {
-        return delteColumn;
+
+    /**
+     * @return The delta of the column this vector describes.
+     */
+    public /*@ pure, helper @*/ int getDeltaColumn() {
+        return deltaColumn;
     }
+    
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + deltaRow;
-        result = prime * result + delteColumn;
+        result = prime * result + deltaColumn;
         return result;
     }
+    
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
     @Override
     public boolean equals(final Object obj) {
         if (this == obj) {
@@ -37,7 +75,7 @@ public final class LocationVector {
         if (deltaRow != other.deltaRow) {
             return false;
         }
-        if (delteColumn != other.delteColumn) {
+        if (deltaColumn != other.deltaColumn) {
             return false;
         }
         return true;
