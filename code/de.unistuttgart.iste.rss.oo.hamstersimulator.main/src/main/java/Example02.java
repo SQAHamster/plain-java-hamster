ef24371b6a0d1708a2e0a2a12a1b4999608fcaf2
Steffen Becker <steffen.becker@informatik.uni-stuttgart.de>
2019-11-12 08:18:09 +0100

    Added Example2

---
+++ code/de.unistuttgart.iste.rss.oo.hamstersimulator.main/src/main/java/Example02.java
@@ -0,0 +1,27 @@
+import de.unistuttgart.iste.rss.oo.hamstersimulator.external.model.Hamster;
+
+class Example02 extends SimpleHamsterGame {
+    Hamster paula;
+
+    /**
+     * Another hamster program. The idea is to create 
+     * Paula close to Paula with
+     * grain in her mouth. She drops it and Paule picks it up.     
+     */
+    @Override
+    void run() {
+        game.initialize();
+        game.displayInNewGameWindow();
+
+        Hamster paula = 
+        		new Hamster(
+        				game.getTerritory(), 
+        				paule.getLocation(), 
+        				paule.getDirection(), 
+        				0);
+
+        paula.putGrain();
+        paule.move();
+        paule.pickGrain();
+    }
+}
