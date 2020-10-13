0efbcda1061bfa8ce1b8352e6d2a962785e9f059
nk-coding <53957498+nk-coding@users.noreply.github.com>
2020-10-13 23:26:28 +0200

    Improved documentation on InputInterface

--- code/de.unistuttgart.iste.rss.oo.hamstersimulator.core/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/adapter/InputInterface.java
+++ code/de.unistuttgart.iste.rss.oo.hamstersimulator.core/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/adapter/InputInterface.java
@@ -4,14 +4,21 @@ import java.util.Optional;
 
 /**
  * Interface to interact with a UI or mock object for reading values from the user.
+ * Warning: subclasses of this interface must support multithreading. <code>readInteger</code>,
+ * <code>readString</code> and <code>showAlert</code> may be invoked from different threads before
+ * the previous invocation returned. It is guaranteed that in this case, <code>abort</code> is called
+ * before the next invocation.
+ *
  * @author Steffen Becker
  *
  */
-public interface InputInterface {
+public interface
+InputInterface {
 
     /**
      * Read an integer value from a user. This blocks until there is
-     * an integer to return or it is aborted
+     * an integer to return or it is aborted.
+     * This must be thread safe
      * @param message The message used in the prompt for the number.
      * @return The integer value read or an empty optional, if aborted.
      */
@@ -20,6 +27,7 @@ public interface InputInterface {
     /**
      * Read a string value from a user. This blocks until there is a
      * String to return or it is aborted
+     * This must be thread safe
      * @param message The message used in the prompt for the string.
      * @return The string value read or an empty optional, if aborted.
      */
@@ -28,6 +36,7 @@ public interface InputInterface {
     /**
      * Inform a user about an abnormal execution aborting.
      * This blocks until it returns or is aborted
+     * This must be thread safe
      * @param t The throwable which lead to aborting the program.
      */
     void showAlert(Throwable t);
@@ -36,7 +45,8 @@ public interface InputInterface {
      * Aborts readInteger, readString or showAlert. This is used due to multi ui systems,
      * where every dialog could return first. Because only one dialog is able to return a
      * value, all other dialogs can be aborted.<br>
-     * This must be thread-safe
+     * This must be thread-safe.
+     * May be called multiple times, even if no input is pending
      */
     void abort();
 }
