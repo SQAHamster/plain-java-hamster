ce508610ea34ab30dc8d29c66df94b0c9d50ee37
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-10-19 21:21:49 +0200

    Added config module and config loaders

---
+++ code/de.unistuttgart.iste.rss.oo.hamstersimulator.config/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module rss.hamster.config {
+    requires com.google.gson;
+    exports de.unistuttgart.iste.rss.oo.hamstersimulator.config;
+    opens de.unistuttgart.iste.rss.oo.hamstersimulator.config;
+}
--- code/de.unistuttgart.iste.rss.oo.hamstersimulator.examples/src/main/java/module-info.java
+++ code/de.unistuttgart.iste.rss.oo.hamstersimulator.examples/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module rss.hamster.examples {
     requires rss.hamster.main;
     requires rss.hamster.ui;
+    requires rss.hamster.config;
 }
