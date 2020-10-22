874c02904687adfba7bb14f4a7e2583cc645f40e
nk-coding <niklask.coding@gmail.com>
2020-10-22 03:31:56 +0200

    fixed "documentation" of Example00

--- code/de.unistuttgart.iste.rss.oo.hamstersimulator.examples/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/examples/Example00.java
+++ code/de.unistuttgart.iste.rss.oo.hamstersimulator.examples/src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/examples/Example00.java
@@ -2,18 +2,15 @@ package de.unistuttgart.iste.rss.oo.hamstersimulator.examples;
 
 import de.unistuttgart.iste.rss.oo.hamstersimulator.external.model.SimpleHamsterGame;
 
-/**
- * First example of a SimpleHamsterGame
- */
+// First example of a SimpleHamsterGame
 public class Example00 extends SimpleHamsterGame {
 
     // Run our first hamster game
     @Override
     protected void run() {
-       // Init UI
-       // Init Territory
-       // 2xMove, 2xPick, Turn around
-       // 2xMove, Turn around
+        // Open UI
+        // 2xMove, 2xPick, Turn around
+        // 2xMove, Turn around
     }
 
 }
