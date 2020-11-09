ad7cbbea944316890a1f20bfa0d36b008df63aa5
nk-coding <niklask.coding@gmail.com>
2020-11-09 14:26:09 +0100

    bugfix: open package to gson

--- code/de.unistuttgart.iste.rss.oo.hamstersimulator.server/src/main/java/module-info.java
+++ code/de.unistuttgart.iste.rss.oo.hamstersimulator.server/src/main/java/module-info.java
@@ -8,6 +8,7 @@ module rss.hamster.server {
     opens de.unistuttgart.iste.rss.oo.hamstersimulator.server.datatypes to com.google.gson;
     opens de.unistuttgart.iste.rss.oo.hamstersimulator.server.datatypes.delta to com.google.gson;
     opens de.unistuttgart.iste.rss.oo.hamstersimulator.server.datatypes.type to com.google.gson;
+    opens de.unistuttgart.iste.rss.oo.hamstersimulator.server.input to com.google.gson;
 
     exports de.unistuttgart.iste.rss.oo.hamstersimulator.server.http.client;
     exports de.unistuttgart.iste.rss.oo.hamstersimulator.server.input;
