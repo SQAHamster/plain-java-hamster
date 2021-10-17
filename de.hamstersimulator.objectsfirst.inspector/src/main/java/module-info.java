bd966ce1e2b492db872ccd0073bd28c4b73a615e
nk-coding <niklask.coding@gmail.com>
2021-10-17 07:44:40 +0200

    fixed dependencies for inspector

--- de.hamstersimulator.objectsfirst.inspector/src/main/java/module-info.java
+++ de.hamstersimulator.objectsfirst.inspector/src/main/java/module-info.java
@@ -2,9 +2,7 @@ module de.hamstersimulator.objectsfirst.inspector {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
-    requires de.hamstersimulator.objectsfirst.core;
-    requires de.hamstersimulator.objectsfirst.ui;
-    requires de.hamstersimulator.objectsfirst.main;
+    requires transitive de.hamstersimulator.objectsfirst.main;
     requires org.controlsfx.controls;
 
     opens de.hamstersimulator.objectsfirst.inspector.ui to javafx.fxml;
