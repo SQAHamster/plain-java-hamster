a0c9a0b2cc5aef59e639e7478cabf52003b43cc0
nk-coding <niklask.coding@gmail.com>
2021-10-29 02:39:33 +0200

    refactored CallMethodDialog and fixed bug that style classes are missing

--- de.hamstersimulator.objectsfirst.inspector/src/main/java/de/hamstersimulator/objectsfirst/inspector/ui/CallMethodDialog.java
+++ de.hamstersimulator.objectsfirst.inspector/src/main/java/de/hamstersimulator/objectsfirst/inspector/ui/CallMethodDialog.java
@@ -5,25 +5,64 @@ import de.hamstersimulator.objectsfirst.inspector.viewmodel.MethodViewModel;
 import de.hamstersimulator.objectsfirst.inspector.viewmodel.ParamViewModel;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
+import javafx.beans.binding.StringExpression;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.scene.control.*;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * Dialog to enter method arguments
+ */
 public class CallMethodDialog extends Dialog<List<Object>> {
 
+    /**
+     * The method which will be called
+     */
+    private final MethodViewModel method;
+    /**
+     * Overall ViewModel for the inspection
+     */
+    private final InspectionViewModel inspectionViewModel;
+    /**
+     * The GridPane with all InputControls to enter arguments for all parameters
+     */
+    private final GridPane contentGrid;
+
+    /**
+     * Creates a new CallMethodDialog
+     * Takes the parameters from the method
+     *
+     * @param method the method which will be called
+     * @param inspectionViewModel the overall ViewModel for the inspection
+     */
     public CallMethodDialog(final MethodViewModel method, final InspectionViewModel inspectionViewModel) {
+        this.method = method;
+        this.inspectionViewModel = inspectionViewModel;
+
         final DialogPane dialogPane = this.getDialogPane();
         dialogPane.getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+        this.contentGrid = this.createChildGridPain();
 
+        this.initTitle();
+        this.createParameterInputs();
+        dialogPane.getScene().getStylesheets().add("style.css");
+    }
+
+    /**
+     * Creates a GridPane used to add arguments
+     *
+     * @return the created GridPane
+     */
+    private GridPane createChildGridPain() {
+        final DialogPane dialogPane = this.getDialogPane();
         final GridPane contentGrid = new GridPane();
         final ColumnConstraints firstColumnConstraints = new ColumnConstraints();
         firstColumnConstraints.setPercentWidth(50);
@@ -32,38 +71,88 @@ public class CallMethodDialog extends Dialog<List<Object>> {
         contentGrid.getColumnConstraints().addAll(firstColumnConstraints, secondColumnConstraints);
         dialogPane.setContent(contentGrid);
         dialogPane.setMaxWidth(300);
+        return contentGrid;
+    }
 
+    /**
+     * Creates InputControls for all parameters and adds them to the contentGrid
+     * Also binds the OK_DONE button to an all valid property, and sets the
+     * result converter
+     */
+    private void createParameterInputs() {
+        final List<InputControl> inputControls = this.createInputControls();
+        this.bindOKButtonToValidation(inputControls);
+        this.createResultConverter(inputControls);
+    }
+
+    /**
+     * Sets the title of the Dialog and adds a label on top of the control
+     * Must be called before createParameterInputs
+     */
+    private void initTitle() {
         final Label methodNameLabel = new Label();
-        methodNameLabel.textProperty().bind((new SimpleStringProperty(this, "titleProp", "Calling method ")).concat(method.nameProperty()));
-        contentGrid.add(methodNameLabel, 0, 0, 2, 1);
+        final StringProperty titleStart = new SimpleStringProperty("Calling method ");
+        methodNameLabel.textProperty().bind(titleStart.concat(method.nameProperty()));
+        this.contentGrid.add(methodNameLabel, 0, 0, 2, 1);
 
+        this.setTitle("Call Method");
+    }
+
+    /**
+     * Creates InputControls for all parameters and adds them to the contentGrid
+     *
+     * @return the list with all InputControls created
+     */
+    private List<InputControl> createInputControls() {
         final List<ParamViewModel> params = method.paramsProperty().get();
-        final List<InputControl> values = new ArrayList<>();
+        final List<InputControl> inputControls = new ArrayList<>();
         for (int i = 0; i < params.size(); i++) {
             final ParamViewModel param = params.get(i);
             final Label nameLabel = new Label();
-            nameLabel.textProperty().bind(param.typeProperty().asString().concat(" ").concat(param.nameProperty()));
+            final StringExpression methodSignature = param.typeProperty().asString().concat(" ")
+                    .concat(param.nameProperty());
+            nameLabel.textProperty().bind(methodSignature);
             contentGrid.add(nameLabel, 0, i+1);
             final InputControl inputControl = new InputControl(param.typeProperty().get(), inspectionViewModel);
             contentGrid.add(inputControl, 1, i+1);
-            values.add(inputControl);
+            inputControls.add(inputControl);
         }
-        final BooleanBinding allValid = Bindings.createBooleanBinding(() ->
-                values.stream().allMatch(inputControl -> inputControl.isValidProperty().get()),
-                values.stream().map(InputControl::isValidProperty).toArray(BooleanProperty[]::new));
-        dialogPane.lookupButton(ButtonType.OK).disableProperty().bind(allValid.not());
+        return inputControls;
+    }
 
+    /**
+     * Binds the OK buttons disableProperty to the negated allValid property
+     *
+     * @param inputControls a list of InputControls which must be all valid
+     */
+    private void bindOKButtonToValidation(final List<InputControl> inputControls) {
+        final DialogPane dialogPane = this.getDialogPane();
+
+        final List<BooleanProperty> dependentProperties = inputControls.stream()
+                .map(InputControl::isValidProperty)
+                .collect(Collectors.toList());
+
+        final BooleanBinding allValid = Bindings.createBooleanBinding(
+                () -> dependentProperties.stream().allMatch(BooleanProperty::get),
+                dependentProperties.toArray(BooleanProperty[]::new));
+        dialogPane.lookupButton(ButtonType.OK).disableProperty().bind(allValid.not());
+    }
+
+    /**
+     * Creates and sets a ResultConverter, which converts the current value of all inputControls
+     *
+     * @param inputControls a list with all InputControls to enter arguments
+     */
+    private void createResultConverter(final List<InputControl> inputControls) {
         this.setResultConverter(dialogButton -> {
             if (dialogButton.getButtonData() == ButtonBar.ButtonData.OK_DONE) {
-                return values.stream().map(InputControl::valueProperty)
+                return inputControls.stream().map(InputControl::valueProperty)
                         .map(ObjectProperty::get)
                         .collect(Collectors.toList());
             } else {
                 return null;
             }
         });
-
-        this.setTitle("Call Method");
     }
 
 }
