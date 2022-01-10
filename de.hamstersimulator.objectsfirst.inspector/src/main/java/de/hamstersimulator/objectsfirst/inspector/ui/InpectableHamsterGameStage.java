b13e3eb1f1810861f1aa3025f364ff078ec0e8b6
nk-coding <niklask.coding@gmail.com>
2022-01-10 18:01:48 +0100

    documented rest of inspection UI code

--- de.hamstersimulator.objectsfirst.inspector/src/main/java/de/hamstersimulator/objectsfirst/inspector/ui/InpectableHamsterGameStage.java
+++ de.hamstersimulator.objectsfirst.inspector/src/main/java/de/hamstersimulator/objectsfirst/inspector/ui/InpectableHamsterGameStage.java
@@ -8,12 +8,23 @@ import javafx.scene.control.SplitPane;
 
 import java.io.IOException;
 
+/**
+ * HamsterGameStage with an additional InspectControl to allow inspection
+ */
 public class InpectableHamsterGameStage extends HamsterGameStage {
 
     private final InspectionViewModel inspectionViewModel;
     private final InspectControl inspectControl;
 
-    public InpectableHamsterGameStage(HamsterGameViewModel hamsterGameViewModel, InspectionViewModel inspectionViewModel) throws IOException {
+    /**
+     * Creates a new HamsterGameStage
+     *
+     * @param hamsterGameViewModel represents the HamsterGame, provided to parent HamsterGameStage
+     * @param inspectionViewModel represents inspection support
+     * @throws IOException if the fxml of the parent control cannot be loaded
+     */
+    public InpectableHamsterGameStage(final HamsterGameViewModel hamsterGameViewModel,
+                                      final InspectionViewModel inspectionViewModel) throws IOException {
         super(hamsterGameViewModel);
 
         this.inspectionViewModel = inspectionViewModel;
